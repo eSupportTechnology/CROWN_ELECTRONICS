9aadb8965b2bdd6863e34eb991da5f71d6f79a1a
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-07-17 12:02:34 +0530

    `Updated admin dashboard sidebar and report views with minor styling and functionality changes.`

--- storage/framework/views/7faba947c99493eb2623987b94558e32.php
+++ storage/framework/views/7faba947c99493eb2623987b94558e32.php
@@ -1,6 +1,5 @@
 <?php $__env->startSection('content'); ?>
 
-<<<<<<< HEAD
     <style>
         .modal-content {
             background-color: #1a1a1a !important;
@@ -30,26 +29,16 @@
         }
     </style>
 
-=======
->>>>>>> 1006f4f51c608c85d6f4e6ac86418b5fc35da199
 <div class="content-header">
     <div>
         <h2 class="content-title card-title">Currencies</h2>
     </div>
     <div>
-<<<<<<< HEAD
         <button type="button" class="btn btn-primary btn-sm rounded" data-bs-toggle="modal" data-bs-target="#addCurrencyModal">Add</button>
     </div>
 </div>
 
 <div class="card mb-4">
-=======
-        <button type="button" class="rounded btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#addCurrencyModal">Add</button>
-    </div>
-</div>
-
-<div class="mb-4 card">
->>>>>>> 1006f4f51c608c85d6f4e6ac86418b5fc35da199
     <div class="card-body">
         <div class="table-responsive">
             <table class="table table-hover">
@@ -100,11 +89,7 @@
 <!-- Add Currency Modal -->
 <div class="modal fade" id="addCurrencyModal" tabindex="-1" aria-labelledby="addCurrencyModalLabel" aria-hidden="true">
     <div class="modal-dialog">
-<<<<<<< HEAD
         <div class="modal-content">
-=======
-        <div class="modal-content" style="background-color: #222736">
->>>>>>> 1006f4f51c608c85d6f4e6ac86418b5fc35da199
             <div class="modal-header">
                 <h5 class="modal-title">Add New Currency</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
